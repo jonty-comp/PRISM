35a533ccea4d034728212ee3447135e6eae23e30
jonty-comp <jontysewell@gmail.com>
2016-09-13 18:25:05 +0100

    Add the ability to download the log mp4 and mjpeg files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var logger = require('winston');
 var express = require('express');
 var stream = require('stream');
 var mjpegCamera = require('mjpeg-camera');
+var glob = require('glob');
+var fs = require('fs');
 
 var processFrame = require('./frameProcessor');
 var logFrame = require('./frameLogger');
@@ -60,4 +62,42 @@ app.get('/cameras', function(req, res) {
 
 	res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(json, null, 3));
+});
+
+app.get('/logs', function(req, res) {
+	var json = cameras.filter(function(camera) {
+		return camera.frameLogger || false;
+	}).map(function(camera) {
+		return {
+			name: camera.name,
+			description: camera.description,
+			url: '/' + camera.name + '.jpg',
+			motion: camera.motion
+		}
+	});
+
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(json, null, 3));
+});
+
+app.get('/logs/:camera/:logfile?', function(req, res) {
+	if(!cameras.some(function(camera) { return (camera.name == req.params.camera); })) {
+		res.sendStatus(404);
+	} else {
+		if(req.params.logfile !== undefined) {
+			fs.access(config.logPath+'/'+req.params.camera+'/'+req.params.logfile, fs.F_OK, function(err) {
+				if (!err) {
+					res.sendFile(config.logPath+'/'+req.params.camera+'/'+req.params.logfile);
+				} else {
+					res.send(404);
+				}
+			});
+		} else {
+			glob(config.logPath+'/'+req.params.camera+'/*.mp4', { nomount: true }, function(er, files) {
+				files = files.map(function(file) { return file.replace(config.logPath+'/'+req.params.camera, ''); });
+				res.setHeader('Content-Type', 'application/json');
+				res.send(JSON.stringify(files, null, 3));
+			});
+		}
+	}
 });
